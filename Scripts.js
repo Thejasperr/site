156a2075832e6410fde1900deeeda3fa50146b4b
Thejasperr <joaoizidrosilva@gmail.com>
2024-07-09 17:55:50 -0300

    v1.3

--- Scripts.js
+++ Scripts.js
@@ -1,20 +1,7 @@
-// início do plugin
-// insira aqui os créditos do autor do plugin
-$.extend($.easing,
-    {
-        easeOutCirc: function (x, t, b, c, d) {
-            return c * Math.sqrt(1 - (t=t/d-1)*t) + b;
-        }
-    });
-    // fim do plugin
-    
-    
-    $("a").click(function(e){
-       e.preventDefault();
-       
-       var anc = this.hash;
-       
-       $("html, body").animate({
-          scrollTop: $(anc).offset().top
-       }, { duration: 2000, easing: 'easeOutCirc'});
-    });
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.quemsomos', {duration:2000});
+sr.reveal('.procedimentos', {rotate: {x:0, y: 80, z: 0}, duration: 1000});
+sr.reveal('.conteudo1', {duration:2000});
+sr.reveal('.conteudo2', {duration:1000});
+sr.reveal('.conteudo3', {duration:2000});
+sr.reveal('.conteudo4', {duration:1000});
