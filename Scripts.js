ffddb222988d1c05ce542a4333da70ec4081078b
Thejasperr <joaoizidrosilva@gmail.com>
2024-07-09 18:01:09 -0300

    v1.4

--- Scripts.js
+++ Scripts.js
@@ -1,6 +1,10 @@
 window.sr = ScrollReveal({reset: true});
 sr.reveal('.quemsomos', {duration:2000});
 sr.reveal('.procedimentos', {rotate: {x:0, y: 80, z: 0}, duration: 1000});
+sr.reveal('.procedimento-1', {duration: 2000});
+sr.reveal('.procedimento-2', {duration: 2000});
+sr.reveal('.procedimento-3', {duration: 2000});
+
 sr.reveal('.conteudo1', {duration:2000});
 sr.reveal('.conteudo2', {duration:1000});
 sr.reveal('.conteudo3', {duration:2000});
